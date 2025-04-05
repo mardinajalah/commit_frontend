ce1b6033dbd54a85878c7b31c316be19978294cc
mardinajalah <tredingb25@gmail.com>
2025-04-05 13:59:20 +0700

    menambahkan folder outh

---
+++ src/components/auth/Login.tsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom"
+
+const Login = () => {
+  return (
+    <Link to="/dashboard" className="btn border-none bg-[#6C0AFF] text-white rounded-2xl">Login</Link>
+  )
+}
+
+export default Login
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,10 +1,11 @@
+import Login from "@/components/auth/Login";
 import Layout from "@/components/layouts/Layout";
 import { Route, Routes } from "react-router-dom";
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path='/' element={<h1>Home</h1>} />
+      <Route path='/' element={<Login />} />
       <Route path='/dashboard/*' element={<Layout />} />
       <Route path='/*' element={<h1>404</h1>} />
     </Routes>
