612463e3240493f8b03140095afec8e996d0701a
syaifmaxl <saifrikdenim@gmail.com>
2025-04-10 15:39:25 +0700

    add 404 not found

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,13 +1,14 @@
 import Login from "@/components/auth/Login";
 import Layout from "@/components/layouts/Layout";
+import NotFound from "@/components/pages/OtherPage/NotFound";
 import { Route, Routes } from "react-router-dom";
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path='/' element={<Login />} />
-      <Route path='/dashboard/*' element={<Layout />} />
-      <Route path='/*' element={<h1>404</h1>} />
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard/*" element={<Layout />} />
+      <Route path="/*" element={<NotFound />} />
     </Routes>
   );
 };
